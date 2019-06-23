3c08050ee9c6878ab911d957eedcbae9d0030299
Sam <deadchinchilla@gmail.com>
2019-06-22 22:39:33 -0700

    All SFX and music successfully integrated, including startup 'music;' software side of elliebox is complete.

--- light_game.py
+++ light_game.py
@@ -4,8 +4,8 @@
 
 from time import sleep
 import subprocess
-# import ButtonHandler
 from Ellie_Button import Ellie_Button
+from music_player import music_player
 
 ###SETME
 LED_pins = [16,20,21]
@@ -15,24 +15,12 @@ class light_game:
     """Class for the actual game."""
     
     buttons = list()
-    mp = None
+    mp = music_player()
     
     def __init__(self):
-        self.mp = self.music_player()
         for i in range(3):
-            b = Ellie_Button(LED_pins[i],button_pins[i],status=False)
+            b = Ellie_Button(LED_pins[i],button_pins[i],False,self.mp)
             self.buttons.append(b)
-
-    def push_button(self,button_index):
-        """Change status of button's light boolean and change actual light accordingly.
-        
-        Args:
-            light_index: The index number of the button that was pressed
-            
-        """
-        self.lights[button_index][1] = not self.lights[button_index][1]
-        print("Pressed button",button_index)
-        self.mp.play()
 
     def check_win_condition(self):
         """Poll each of the lights to see if they're on. If all three are on, run win method."""
@@ -44,23 +32,38 @@ class light_game:
     def win(self):
         """Play win music, rotate through the lights which one should be on (ten total runthroughs), turn off all lights and kill the music."""
         sleep(0.5)
-        self.mp.play(0)
-        for i in range(1): #TODO: Do ten times
+        self.mp.play(win=True)
+        for i in range(6): #TODO: Do ten times
             for j in range(3): #Light up the lights one at a time
                 for k in range(3): #Go through each light and only turn it on if it's the light in this cycle, otherwise turn it off
                     if j==k:
                         self.buttons[k].showlight(True)
                     else:
                         self.buttons[k].showlight(False)
-                sleep(1)
+                sleep(0.5)
         for light in self.buttons:
             light.is_lit = False
             light.showlight()
-        #Check whether the music has stopped playing
-
+            
+    def startup(self):
+        """Light up the lights in order, play the specified 3 sound effects, turn lights off."""
+        print("STARTUP")
+        for i in range(3):
+            self.buttons[i].light.on()
+            self.buttons[i].showlight(True)
+            self.buttons[i].mp.play_specific(i+1)
+            sleep(0.5)
+        sleep(2)
+        for i in range(3):
+            self.buttons[i].light.off()
+            self.buttons[i].showlight()
+        
 
 if __name__ == "__main__":
     lg = light_game()
+    sleep(1)
+    lg.startup()
+    print("Let's play")
     while True:
         try:
             lg.check_win_condition()
--- music_player.py
+++ music_player.py
@@ -1,30 +1,30 @@
 import subprocess
-import random
+from random import randrange
 
 class music_player:
-        """Music control."""
-        sfx = 1
-        p = None
+    """Music control."""
+    sfx = 7
 
-        def play(self, win=False):
-            """Play a sound clip.
-            
-            Args:
-                clip: The index number of the clip to play
-
-            """
-            if win:
-                print("Play win music")
-            else:
-                print("Play sound effect")
-                cmd = "sudo omxplayer /home/pi/Desktop/elliebox/" + str(randint(0,sfx)) + ".mp3 &"                  # lays out the complete string that you'll pass to bash when you call the command as a subprocess   
-                p = subprocess.Popen(cmd, shell=True)
-            # Do some stuff to create a command to send to subprocess.Popen as self.p
-            # Send the command
-            # Wait for the duration of the clip
-            self.stop()
-            # Return exit code or something here so that the calling function knows it's done playing? Really just necessary for the "win" function
+    def play(self, win=False):
+        """Play a sound clip.
         
-        def stop(self):
-            print("stop clip")
-            #~ self.p.terminate()
+        Args:
+            win: Is this win condition?
+
+        """
+        if win:
+            cmd = "omxplayer /home/pi/Desktop/elliebox/SFX/elliebox_win.wav &"
+            p = subprocess.Popen(cmd, shell=True)
+        else:
+            cmd = "omxplayer /home/pi/Desktop/elliebox/SFX/elliebox_sfx_0" + str(randrange(1,self.sfx+1)) + ".wav &"
+            p = subprocess.Popen(cmd, shell=True)
+            
+    def play_specific(self,num):
+        """Play a specific sound clip.
+        
+        Args:
+            num: The number of the file you want (must be between 1 and 7, inclusive)
+        
+        """
+        cmd = "omxplayer /home/pi/Desktop/elliebox/SFX/elliebox_sfx_0" + str(num) + ".wav &"
+        p = subprocess.Popen(cmd, shell=True)
